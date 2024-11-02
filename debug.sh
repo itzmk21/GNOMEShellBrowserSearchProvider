41842230f3a57ce8574eeeeb6d0bb3ddd5874f01
Mussa Mipawa <mepowerleo10@gmail.com>
2024-11-02 16:49:44 +0300

    Improve packaging and debug scripts

--- debug.sh
+++ debug.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
 
+./package.sh
+
 # export G_MESSAGES_DEBUG=all
 export MUTTER_DEBUG_DUMMY_MODE_SPECS=1366x768
 # export SHELL_DEBUG=all
--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-OUT_DIR=$(pwd)
-TMP_DIR='/tmp/BrowserSearchProvider@mepowerleo10.github.io'
+WORKING_DIR=$(pwd)/output/
+TMP_DIR=$WORKING_DIR/BrowserSearchProvider@mepowerleo10.github.io
 if [ -d $TMP_DIR ]; then
   rm -r $TMP_DIR
 fi
@@ -12,4 +12,4 @@ cp metadata.json $TMP_DIR
 cp README.md $TMP_DIR
 
 cd $TMP_DIR
-zip -r $OUT_DIR/BrowserSearchProvider@mepowerleo10.github.io.zip .
+zip -r $WORKING_DIR/BrowserSearchProvider@mepowerleo10.github.io.zip .
